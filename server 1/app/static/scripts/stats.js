7311b2f0beb97f1e4f1d05e48a4c620f265b5abd
Ed Perez <edjo.perez@gmail.com>
2024-10-04 16:03:19 -0500

    server 1: Added final styles

--- server 1/app/static/scripts/stats.js
+++ server 1/app/static/scripts/stats.js
@@ -3,9 +3,9 @@ $(()=>{
     $.get("/api/dashboard/stats?command=cat ../assets/stats.json", (res)=>{
       const stats = JSON.parse(res);
       $("#lastUpdate p").text(stats.lastUpdate)
-      $("#connected-users p").text(stats.connectedUsers);
-      $("#new-orders p").text(stats.newOrders);
-      $("#total-trafic p").text(stats.totalTraffic)
+      $(".card-visitors .data").text(stats.connectedUsers);
+      $(".card-orders .data").text(stats.newOrders);
+      $(".card-traffic .data").text(stats.totalTraffic)
     });
   });
   
